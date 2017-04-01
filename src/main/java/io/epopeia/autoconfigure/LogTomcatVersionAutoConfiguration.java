1edddad3098221e61f6dab2b92865623fe83ce25
Fernando Amaral <fernandoluizjr@gmail.com>
2017-04-01 02:41:05 -0300

    created a custom auto configuration

---
+++ src/main/java/io/epopeia/autoconfigure/LogTomcatVersionAutoConfiguration.java
@@ -0,0 +1,23 @@
+package io.epopeia.autoconfigure;
+
+import javax.annotation.PostConstruct;
+
+import org.apache.catalina.startup.Tomcat;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnClass(Tomcat.class) // only if tomcat class is on classpath
+@ConditionalOnProperty(name = "log-tomcat-version", matchIfMissing = true)
+public class LogTomcatVersionAutoConfiguration {
+
+	private static Log logger = LogFactory.getLog(LogTomcatVersionAutoConfiguration.class);
+	
+	@PostConstruct
+	public void logTomcatVersion() {
+		logger.info("\n\n\nTomcat v" + Tomcat.class.getPackage().getImplementationVersion() + "\n\n");
+	}
+}
