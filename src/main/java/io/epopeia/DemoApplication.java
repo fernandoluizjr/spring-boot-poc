bd66320e504554e5d42978c99e5d0a89af7814dd
Fernando Amaral <fernandoluizjr@gmail.com>
2017-04-01 02:21:34 -0300

    adding comments to clarify the @SpringBootApplication annotation parts

--- src/main/java/io/epopeia/DemoApplication.java
+++ src/main/java/io/epopeia/DemoApplication.java
@@ -3,6 +3,9 @@ package io.epopeia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// @Configuration (the primary one)
+// @EnableAutoConfiguration (use exclude to disable auto-configuration classes)
+// @ComponentScan (from this base package)
 @SpringBootApplication
 public class DemoApplication {
 
