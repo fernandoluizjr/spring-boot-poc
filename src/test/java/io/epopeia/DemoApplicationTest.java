b412d0444b47d995da9362976ae94c68862f1835
Fernando Amaral <fernandoluizjr@gmail.com>
2017-04-08 13:57:47 -0300

    enhancement of explanation of unit tests

--- src/test/java/io/epopeia/DemoApplicationTest.java
+++ src/test/java/io/epopeia/DemoApplicationTest.java
@@ -4,8 +4,9 @@ package io.epopeia;
  * Package unit has tests without spring container (using new operator), but
  * uses some support mock and utilities classes from spring-test module such as
  * MockEnvironment, MockPropertySource, mocks for web (servlet, session etc),
- * AOP utilities, JDBC utilities etc. The idea here is use some test framework
- * like jUnit with these support classes.
+ * Reflection utilities (Ex: set a value for an attribute value), AOP utilities
+ * (Ex: extract target object from its proxy), MVC utilities etc. The idea here
+ * is to use some test framework like jUnit with these support classes.
  */
 public class DemoApplicationTest {
 
