e073670ecb47c6c30b8ab49b476255c7c594159e
Fernando Amaral <fernandoluizjr@gmail.com>
2017-04-08 00:19:31 -0300

    using property placeholder resolver to inject a value

--- src/main/java/io/epopeia/web/EnvironmentController.java
+++ src/main/java/io/epopeia/web/EnvironmentController.java
@@ -1,5 +1,6 @@
 package io.epopeia.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EnvironmentController {
 
 	private final Environment containerEnvironment;
+	private final String value;
 
-	public EnvironmentController(final Environment containerEnvironment) {
+	public EnvironmentController(final Environment containerEnvironment,
+			@Value("${value.injected}") final String value) {
 		this.containerEnvironment = containerEnvironment;
+		this.value = value;
 	}
 
 	@RequestMapping("/environment")
@@ -21,5 +25,10 @@ public class EnvironmentController {
 	@RequestMapping("/profile-property")
 	public String profileProperty() {
 		return containerEnvironment.getProperty("profile.property");
+	}
+	
+	@RequestMapping("/value-injected")
+	public String valueInjected() {
+		return this.value;
 	}
 }
