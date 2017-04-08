652e3df89ea4754a2bdeb3dca5c3e067bd391d3a
Fernando Amaral <fernandoluizjr@gmail.com>
2017-04-08 17:39:49 -0300

    enhancement of explanation on root test class

--- src/test/java/io/epopeia/DemoApplicationTest.java
+++ src/test/java/io/epopeia/DemoApplicationTest.java
@@ -7,6 +7,12 @@ package io.epopeia;
  * Reflection utilities (Ex: set a value for an attribute value), AOP utilities
  * (Ex: extract target object from its proxy), MVC utilities etc. The idea here
  * is to use some test framework like jUnit with these support classes.
+ * 
+ * Beyond mocks and util classes for unit testing (also they can be used for
+ * integration testing), the idea here is load the spring context container.
+ * This loaded container is cached (may be restarted) for all tests, resolve
+ * dependency injections, provides transaction management and additional class
+ * support such as JdbcTemplate, JdbcTestUtils etc.
  */
 public class DemoApplicationTest {
 
