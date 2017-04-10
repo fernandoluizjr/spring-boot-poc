a2bf04c7b66857026b048f437060fe94da6ebdaf
Fernando Amaral <fernandoluizjr@gmail.com>
2017-04-10 01:06:05 -0300

    adding web mocks for a web context

--- src/test/java/io/epopeia/integration/JunitWithSpringContext.java
+++ src/test/java/io/epopeia/integration/JunitWithSpringContext.java
@@ -5,20 +5,41 @@ import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.MethodMode;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.request.ServletWebRequest;
 
 @RunWith(SpringRunner.class) // the spring runner
-@ContextConfiguration // set how to load the container (default here)
-@WebAppConfiguration // also set this container as a web container
+@ContextConfiguration // set how to load the container (config beans)
+@WebAppConfiguration // also set this container with a MockServletContext
+						// default resources are set to "file:src/main/webapp"
 public class JunitWithSpringContext {
 
-	@Autowired // runner loads the web context and caches it
-	public WebApplicationContext springWebContext;
+	@Autowired
+	public WebApplicationContext springWebContext; // cached
+
+	@Autowired
+	public MockServletContext servletContext; // cached
+
+	@Autowired
+	public MockHttpSession session;
+
+	@Autowired
+	public MockHttpServletRequest request;
+
+	@Autowired
+	public MockHttpServletResponse response;
+
+	@Autowired
+	public ServletWebRequest webRequest;
 
 	@Test
 	public void springMustBeLoadedAndCached() {
