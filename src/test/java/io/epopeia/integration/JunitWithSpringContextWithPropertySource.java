58d0c86c313d0e9b2d071258126493e44f7552ff
Fernando Amaral <fernandoluizjr@gmail.com>
2017-04-10 00:25:04 -0300

    enhancement of test with properties

--- src/test/java/io/epopeia/integration/JunitWithSpringContextWithPropertySource.java
+++ src/test/java/io/epopeia/integration/JunitWithSpringContextWithPropertySource.java
@@ -11,9 +11,10 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration
-@TestPropertySource("/test.properties") // or also inline as below:
-// @TestPropertySource(properties = { "timezone = GMT", "port: 4242" })
+@ContextConfiguration // properties from @Configuration classes is also loaded
+						// but Test properties has higher precedence overriding
+@TestPropertySource( // defaults to <class name>.properties if omitted
+locations = "/test.properties", properties = { "timezone = GMT", "port: 4242" })
 public class JunitWithSpringContextWithPropertySource {
 
 	@Value("${property.only.defined.in.test}")
